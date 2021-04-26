ca0d8f83c61eb698576fb8ffda839273c578b1a6
Everton Pereira <evertonsouls@gmail.com>
2021-04-26 23:23:39 +0200

    Statics pages

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import { GetStaticProps } from 'next';
+import { FiUser, FiCalendar } from 'react-icons/fi';
 
 import { getPrismicClient } from '../services/prismic';
 
@@ -24,9 +25,66 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home(): JSX.Element {
+  return (
+    <div className={styles.container}>
+      <img src="/logo.svg" alt="spacetraveling." />
+
+      <main className={styles.content}>
+        <div className={styles.posts}>
+          <a>
+            <strong>Como utilizar Hooks</strong>
+            <p>Pensando em sincronização em vez de ciclos de vida.</p>
+            <div className={commonStyles.info}>
+              <time>
+                <FiCalendar size={20} />
+                15 Mar 2021
+              </time>
+              <span>
+                <FiUser size={20} />
+                Joseph Oliveira
+              </span>
+            </div>
+          </a>
+
+          <a>
+            <strong>Como utilizar Hooks</strong>
+            <p>Pensando em sincronização em vez de ciclos de vida.</p>
+            <div>
+              <time>
+                <FiCalendar size={20} />
+                15 Mar 2021
+              </time>
+              <span>
+                <FiUser size={20} />
+                Joseph Oliveira
+              </span>
+            </div>
+          </a>
+
+          <a>
+            <strong>Como utilizar Hooks</strong>
+            <p>Pensando em sincronização em vez de ciclos de vida.</p>
+            <div>
+              <time>
+                <FiCalendar size={20} />
+                15 Mar 2021
+              </time>
+              <span>
+                <FiUser size={20} />
+                Joseph Oliveira
+              </span>
+            </div>
+          </a>
+        </div>
+
+        <div className={styles.loadMore}>
+          <a href="/">Carregar mais posts</a>
+        </div>
+      </main>
+    </div>
+  );
+}
 
 // export const getStaticProps = async () => {
 //   // const prismic = getPrismicClient();
--- src/pages/post/[slug].tsx
+++ src/pages/post/[slug].tsx
@@ -1,4 +1,5 @@
 import { GetStaticPaths, GetStaticProps } from 'next';
+import { FiUser, FiCalendar, FiClock } from 'react-icons/fi';
 
 import { getPrismicClient } from '../../services/prismic';
 
@@ -26,9 +27,58 @@ interface PostProps {
   post: Post;
 }
 
-// export default function Post() {
-//   // TODO
-// }
+export default function Post(): JSX.Element {
+  return (
+    <div className={styles.container}>
+      <header>
+        <img src="/logo.svg" alt="spacetraveling." />
+      </header>
+
+      <figure className={styles.banner}>
+        <img src="/banner.png" alt="Eat Sleep Code Repeat" />
+      </figure>
+
+      <main className={styles.content}>
+        <article className={styles.post}>
+          <header>
+            <h1>Criando um app CRA do zero</h1>
+            <div className={commonStyles.info}>
+              <time>
+                <FiCalendar size={20} />
+                15 Mar 2021
+              </time>
+              <span>
+                <FiUser size={20} />
+                Joseph Oliveira
+              </span>
+              <span>
+                <FiClock size={20} />4 min
+              </span>
+            </div>
+          </header>
+
+          <div className={styles.postContent}>
+            <h2>Proin et varius</h2>
+
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+
+            <p>
+              Nullam dolor sapien, vulputate eu diam at, condimentum hendrerit
+              tellus. Nam facilisis sodales felis, pharetra pharetra lectus
+              auctor sed.
+            </p>
+
+            <p>
+              Ut venenatis mauris vel libero pretium, et pretium ligula
+              faucibus. Morbi nibh felis, elementum a posuere et, vulputate et
+              erat. Nam venenatis.
+            </p>
+          </div>
+        </article>
+      </main>
+    </div>
+  );
+}
 
 // export const getStaticPaths = async () => {
 //   const prismic = getPrismicClient();
